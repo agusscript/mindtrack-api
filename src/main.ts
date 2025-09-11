b146c52ac0363476c9d1b8f37205cd8440ebf449
agusscript <agus.sanchez.240@gmail.com>
2025-09-10 22:58:09 -0300

    Add bootstrap main config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+
+  const globalPrefix: string = configService.get('server.prefix') as string;
+  app.setGlobalPrefix(globalPrefix);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const port: number = configService.get('server.port') as number;
+  await app.listen(port);
 }
 bootstrap();
