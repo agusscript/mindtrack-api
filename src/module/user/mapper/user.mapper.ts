9f088bbc3ea88cda2c10d4685e9276753d485579
agusscript <agus.sanchez.240@gmail.com>
2025-09-11 22:10:51 -0300

    Add service layer for user module

---
+++ src/module/user/mapper/user.mapper.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { ICreateUserDto } from '../dto/create-user.dto.interface';
+import { User } from '../entity/user.entity';
+
+@Injectable()
+export class UserMapper {
+  fromCreateDtoToEntity(createUserDto: ICreateUserDto): User {
+    const user = new User();
+
+    user.name = createUserDto.name;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+
+    return user;
+  }
+}
---
+++ src/module/user/service/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository/user.repository';
+import { User } from '../entity/user.entity';
+import { ICreateUserDto } from '../dto/create-user.dto.interface';
+import { UserMapper } from '../mapper/user.mapper';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly userMapper: UserMapper,
+  ) {}
+
+  async getOneById(id: number): Promise<User> {
+    return await this.userRepository.getOneById(id);
+  }
+
+  async getOneByEmail(email: string): Promise<User> {
+    return await this.userRepository.getOneByEmail(email);
+  }
+
+  async create(createUserDto: ICreateUserDto): Promise<User> {
+    const mappedUser = this.userMapper.fromCreateDtoToEntity(createUserDto);
+    return await this.userRepository.create(mappedUser);
+  }
+}
