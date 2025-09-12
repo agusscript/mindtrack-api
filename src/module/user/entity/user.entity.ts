9eff303ad348ce8293bd2ec5e4c0585aad89e348
agusscript <agus.sanchez.240@gmail.com>
2025-09-11 22:08:52 -0300

    Add initial user entity

---
+++ src/module/user/entity/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+}
