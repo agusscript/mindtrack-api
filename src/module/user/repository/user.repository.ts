d56e3411e11a7eee5f2ac8a8a59473f8b505b74e
agusscript <agus.sanchez.240@gmail.com>
2025-09-11 22:11:17 -0300

    Add repository layer for user module

---
+++ src/module/user/repository/user.repository.ts
@@ -0,0 +1,47 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entity/user.entity';
+import { Repository } from 'typeorm';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+export class UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+  ) {}
+
+  async getOneById(id: number): Promise<User> {
+    const user = await this.repository.findOne({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    return user;
+  }
+
+  async getOneByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+
+    return user;
+  }
+
+  async create(user: User): Promise<User> {
+    const existingUser = await this.repository.findOne({
+      where: { email: user.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    return await this.repository.save(user);
+  }
+}
