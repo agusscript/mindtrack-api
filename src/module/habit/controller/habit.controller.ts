d0b4b5f1e0665ef7cda56d0c9bb6f434b47b0659
agusscript <agus.sanchez.240@gmail.com>
2025-11-24 20:06:20 -0300

    Add protection to task and habit routes with authentication guard

--- src/module/habit/controller/habit.controller.ts
+++ src/module/habit/controller/habit.controller.ts
@@ -7,16 +7,19 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { HabitService } from '../service/habit.service';
 import { Habit } from '../entity/habit.entity';
 import { CreateHabitDto } from '../dto/create-habit.dto';
 import { UpdateHabitDto } from '../dto/update-habit.dto';
+import { AuthenticationGuard } from 'src/module/authentication/guard/authentication.guard';
 
 @Controller('habit')
 export class HabitController {
   constructor(private readonly habitService: HabitService) {}
 
+  @UseGuards(AuthenticationGuard)
   @Get('user/:userId')
   async getAllByUserId(
     @Param('userId', ParseIntPipe) userId: number,
@@ -24,11 +27,13 @@ export class HabitController {
     return this.habitService.getAllByUserId(userId);
   }
 
+  @UseGuards(AuthenticationGuard)
   @Post()
   async create(@Body() createHabitDto: CreateHabitDto): Promise<Habit> {
     return this.habitService.create(createHabitDto);
   }
 
+  @UseGuards(AuthenticationGuard)
   @Patch(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -37,6 +42,7 @@ export class HabitController {
     return this.habitService.update(id, updateHabitDto);
   }
 
+  @UseGuards(AuthenticationGuard)
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.habitService.delete(id);
--- src/module/task/controller/task.controller.ts
+++ src/module/task/controller/task.controller.ts
@@ -7,16 +7,19 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { TaskService } from '../service/task.service';
 import { CreateTaskDto } from '../dto/create-task.dto';
 import { Task } from '../entity/task.entity';
 import { UpdateTaskDto } from '../dto/update-task.dto';
+import { AuthenticationGuard } from 'src/module/authentication/guard/authentication.guard';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @UseGuards(AuthenticationGuard)
   @Get('user/:userId')
   async getAllByUserId(
     @Param('userId', ParseIntPipe) userId: number,
@@ -24,11 +27,13 @@ export class TaskController {
     return this.taskService.getAllByUserId(userId);
   }
 
+  @UseGuards(AuthenticationGuard)
   @Post()
   async create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskService.create(createTaskDto);
   }
 
+  @UseGuards(AuthenticationGuard)
   @Patch(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -37,6 +42,7 @@ export class TaskController {
     return this.taskService.update(id, updateTaskDto);
   }
 
+  @UseGuards(AuthenticationGuard)
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.taskService.delete(id);
